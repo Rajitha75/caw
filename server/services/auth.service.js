b519bfc79434b2ea219e2e1055deea35002503d8
rajitha ganji <rganji@bodhtree.com>
2019-06-26 22:07:48 +0530

    changes updates

--- server/services/auth.service.js
+++ server/services/auth.service.js
@@ -1,8 +1,10 @@
 const jwt=require('jsonwebtoken');
 const config = require('../config/config');
+
 function checktoken(req, res, next){
 let token = req.headers['x-access-token'] || req.headers['authorization']; 
 try{
+  if(req.headers.authorization){
 const token = req.headers.authorization.split(" ")[1];
 jwt.verify(token, config.secretkey, function (err, payload) {
     if (payload) {
@@ -13,10 +15,14 @@ res.status(200).send({
       console.log('error');
    }
 })
+  }else{
+    console.log('Not an authorized user');
+    return false;
+  }
 }catch(e){
 console.log(e);
 }
-return true;
+return next();
 
 }
 
